1ece963182f5d1469a0ce85c95db8ec8113a4dc0
William Vitorino <williamvitorino3@gmail.com>
2018-05-14 11:17:32 -0300

    Função de ordenação;

---
+++ S6/CAA/ordenar.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// void swap(int* from, int* to) {
+    // Ta bugado
+//     int swap;
+//     swap = to*;
+//     to* = from*;
+//     from* = swap;
+// }
+
+void ordenar(int A[], int n){
+    // Encontrar o menor elemento e fazer swap com o da menor posicao não ordenada.
+    int pos_ord = 0;
+    do {
+        int menor = A[pos_ord], pos = pos_ord;
+        int i;
+        for(i = pos_ord; i < n; i++) {
+            if(A[i] < menor) {
+                menor = A[i];
+                pos = i;
+            }
+        }
+        // swap(&A[pos_ord], &menor);
+        int swap;
+        swap = A[pos_ord];
+        A[pos_ord++] = menor;
+        A[pos] = swap;
+    }while(pos_ord != n);
+    int i;
+    for(i = 0; i < n; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {5, 4, 3, 2, 1};
+    ordenar(arr, 5);
+    return 0;
+}
