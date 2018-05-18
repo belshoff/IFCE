8fc4add50acc2eeba6e48f999604e39c185cde61
William Vitorino <williamvitorino3@gmail.com>
2018-05-18 15:36:47 -0300

    Questao 91

--- S6/CAA/codCad/91.cpp
+++ S6/CAA/codCad/91.cpp
@@ -4,7 +4,24 @@
 int main(int argc, char const *argv[])
 {
     int h, p, f, d;
+    char roda[16] = {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'};
+    char arr[3];
+    int last = 0;
+
     scanf("%d %d %d %d", &h, &p, &f, &d);
-    
+    roda[h] = 'h';
+    roda[p] = 'p';
+    roda[f] = 'f';
+    for (int i = 0; i < 16; i++) {
+      if(roda[i] != '.')
+      arr[last++] = roda[i];
+    }
+    if(arr[0] == 'p') {
+      printf("%c\n", arr[1] == 'h' ? (d == -1 ? 'S' : 'N') : (d == 1 ? 'S' : 'N'));
+    } else if(arr[0] == 'h') {
+      printf("%c\n", arr[1] == 'f' ? (d == -1 ? 'S' : 'N') : (d == 1 ? 'S' : 'N'));
+    } else if(arr[0] == 'f') {
+      printf("%c\n", arr[1] == 'p' ? (d == -1 ? 'S' : 'N') : (d == 1 ? 'S' : 'N'));
+    }
     return 0;
 }
