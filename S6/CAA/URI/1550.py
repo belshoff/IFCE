6620ef49a4a6eb8d4ab91ac2b3c66251f17cd735
William Vitorino <williamvitorino3@gmail.com>
2018-10-10 00:10:58 -0300

    Questão 1550.py

--- S6/CAA/URI/1550.py
+++ S6/CAA/URI/1550.py
@@ -1,13 +1,31 @@
-vis = [False]*10100
+def find(a, b):
+    seen = [-1]*10010
+    lista = [a]
+    seen[a] += 1
+    while(len(lista)):
+        if(seen[b] != -1): return seen[b]
+        frst = lista[0]
+        del lista[0]
+        inv = int(str(frst)[::-1])
+        if(len(seen) > frst+1):
+            if(seen[frst+1] == -1):
+                seen[frst+1] = seen[frst]+1
+                lista.append(frst+1)
+        if(len(seen) > inv):
+            if(seen[inv] == -1):
+                seen[inv] = seen[frst]+1
+                lista.append(inv)
+    return seen[b]
 
-print(len(vis))
+for t in range(int(input())):
+    a, b = map(int, input().split())
+    print(find(a, b))
 
-def bfs(src, des):
-    if(vis[src]): return 100000
-    vis[src] = True
-    if(src == des): return 1
-    return 1 + min(bfs(src+1, des), bfs(int(str(src)[::-1]), des))
+# Idéia:
+#     Ao invés de usar a lista de visitado como Booleano, utilizar como um contador.
+#     A posição que eu estou vendo atualmente, é encrementada de acordo com o contador
+#     da posição pela qual eu cheguei até o elemento atual.
+#     Assim, quando a posição destino for vista, ela terá o contador do caminho acululado.
 
-for t  in range(int(input())):
-    src, des = map(int, input().split())
-    print(bfs(src, des))
+# Análise Assintótica:
+#     O(n)
---
+++ S6/CAA/UVA/341.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
+const int MAXN = 15;
+
+int n;
+vector<pii> adj[MAXN];
+
+int main() {
+  while(cin >> n) 
+  {
+    if(n == 0) break;
+  }
+  return 0;
+}
